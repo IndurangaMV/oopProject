24222926c149bc4c68f71391f6978f28374d3c11
induranga <induranga21297@gmail.com>
2025-01-25 05:14:20 -0800

    a

---
+++ Project/src/main/MedicalCenter.java
@@ -0,0 +1,15 @@
+ public class MedicalCenter {
+    String studentName;
+    String StudentNumber;
+    String requestDate;
+    String userName;
+    String Password;
+
+    void RequestMedical(){
+
+     }
+     void ApproveMedical(){
+
+     }
+
+}
